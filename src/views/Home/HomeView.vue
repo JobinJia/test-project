d69a0034dee5b636e3c61b4d36abe7dcd09ced80
Jobin <425605679@qq.com>
2024-06-14 15:19:56 +0800

    Initial commit

---
+++ src/views/Home/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useCounterStore } from '@/stores/counter'
+import MaterialSymbolsAdd from '~icons/material-symbols/add'
+
+const counter = useCounterStore()
+
+const { doubleCount, count } = storeToRefs(counter)
+</script>
+
+<template>
+  <main>
+    <h2 class="w-full flex">
+      Home Page
+    </h2>
+    <button
+      class="flex w-auto p-2 text-4 border-hidden bg-cyan border-rd-1 outline-hidden cursor-pointer hover-bg-emerald hover-text-white"
+      @click="counter.increment"
+    >
+      <MaterialSymbolsAdd />
+      Increment
+    </button>
+    <p>count => {{ count }}</p>
+    <p>doubleCount => {{ doubleCount }}</p>
+    <!-- svg 加载一个图片 -->
+  </main>
+</template>
